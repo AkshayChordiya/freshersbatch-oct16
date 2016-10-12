4d2ebd7dd0555cde3861417985ae4562a4a3e87f
Aditya Kamble <adityakamble49@gmail.com>
2016-10-12 12:33:27 +0530

    Fixed Folder Structure

---
+++ Assignments/SampleProject/src/com/example/Test.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello from Sample project");
+		System.out.println("Hello from Aditya");
+	}
+}
